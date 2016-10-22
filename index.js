8343e1b877158286f16dcc2f50bf61c6667e01c2
Juan Soto <juan@juansoto.me>
2016-10-22 16:01:27 -0400

    Replace `is-fn` with `typeof`

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 'use strict';
 
-const isFn = require('is-fn');
-
 module.exports = (cond, doIf, doElse) => val => {
-	if (isFn(cond)) {
+	if (typeof cond === 'function') {
 		return cond(val) ? doIf(val) : (doElse ? doElse(val) : val);
 	}
 
