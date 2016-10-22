9311a5f8593746afbd6210490937ac6ff7788311
Sindre Sorhus <sindresorhus@gmail.com>
2016-10-22 13:10:19 +0700

    minor test tweak

--- test.js
+++ test.js
@@ -1,25 +1,27 @@
 import test from 'ava';
 import m from './';
 
+const fixture = Symbol('fixture');
+
 test('if', async t => {
-	const val = await Promise.resolve('unicorn')
+	const val = await Promise.resolve(fixture)
 		.then(m(true, () => 'if', () => 'else'));
 
 	t.is(val, 'if');
 });
 
 test('else', async t => {
-	const val = await Promise.resolve('unicorn')
+	const val = await Promise.resolve(fixture)
 		.then(m(false, () => 'if', () => 'else'));
 
 	t.is(val, 'else');
 });
 
 test('passthrough', async t => {
-	const val = await Promise.resolve('unicorn')
+	const val = await Promise.resolve(fixture)
 		.then(m(false, () => 'if'));
 
-	t.is(val, 'unicorn');
+	t.is(val, fixture);
 });
 
 test('composability', async t => {
