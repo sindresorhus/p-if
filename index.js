b88e4dbf3f13b2490f2a9804352f7b56733a27d5
Juan Soto <juan@juansoto.me>
2016-10-30 13:22:41 -0400

    Abstract common function away

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 'use strict';
 
 module.exports = (cond, doIf, doElse) => val => {
+	const chooseFn = bool => bool === true ? doIf(val) : (doElse ? doElse(val) : val);
+
 	if (typeof cond === 'function') {
-		return Promise.resolve(cond(val))
-			.then(bool => bool === true ? doIf(val) : (doElse ? doElse(val) : val));
+		return Promise.resolve(cond(val)).then(chooseFn);
 	}
 
-	return cond ? doIf(val) : (doElse ? doElse(val) : val);
+	return chooseFn(cond);
 };
+
